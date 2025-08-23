227214640231ab84af1a1dbdc035d36310a29299
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-08-24 01:11:57 +0300

    Agg swagger docs

--- fitness_tracker/fitness_tracker/urls.py
+++ fitness_tracker/fitness_tracker/urls.py
@@ -18,19 +18,37 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/workouts/', include('workouts.urls')),
+    path('api/nutrition/', include('nutrition.urls')),
+    path('api/summaries/', include('summaries.urls')),
 
-    #JWT endpoints
+    # JWT endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Auth for browsable API
     path('api-auth/', include('rest_framework.urls')),  
+]
 
-    path('api/', include('workouts.urls')),
-    path('api/', include('nutrition.urls')),
-    path('api/', include('summaries.urls')),
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Fitness Tracker API",
+      default_version='v1',
+      description="Track workouts & meals",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
